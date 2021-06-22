4cfed5198404404c92116a0d0811b786e8f13446
Lex95 <andrea.lega1995@gmail.com>
2021-06-22 16:53:56 +0200

    aggiunta pacchetto sass e risoluzione esercizio

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,13 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href={{ asset("css/app.css") }}>
     <title>Document</title>
 </head>
 <body>
-    <ul>
+    <div class="container-flex">
         @foreach($moviesList as $movie)
-        <li>{{ $movie->title }}</li>
+        <div class="item">
+            <ul>
+                <li><strong>{{ $movie->title }}</strong></li>
+                <li>{{ $movie->original_title }}</li>
+                <li>{{ $movie->date }}</li>
+                <li>Vote: {{ $movie->vote }}</li>
+            </ul>
+        </div>
         @endforeach
-    </ul>
+    </div>
 </body>
 </html>
